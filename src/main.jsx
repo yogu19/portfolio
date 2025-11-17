d5843915cda39dff73e8ae41ef96f4e471ef905f
Bhishmesh <bhishmeshdhabhai19@gmail.com>
2025-11-17 10:20:47 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
